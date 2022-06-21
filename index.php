760ed02e4e36b0ebc66d159cd06ef85963b721ab
normal-pcer <tianyafei2009@outlook.com>
2022-06-21 20:39:44 +0800

    quqi2's commit

--- index.php
+++ index.php
@@ -4,12 +4,34 @@
     <?php
     // Read config.json
     $config = json_decode(file_get_contents('config.json'), true);
+    //act,cookie
+    $act = $_GET;
+    $cok = $_COOKIE;
     ?>
     <title><?php echo $config['title']; ?></title>
     <meta charset="utf-8">
-
-    <!-- Load stylesheets -->
+    <!-- 载入依赖的样式 -->
     <link rel="stylesheet" href="./style.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://jsd.miaowuawa.cn/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://jsd.miaowuawa.cn/npm/bootstrap@3.4.1/dist/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://jsd.miaowuawa.cn/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+    <!--sweetalert-->
+    <script src="https://jsd.miaowuawa.cn/npm/sweetalert2@8"></script>
+    <script src="sweetalert2.all.min.js"></script>
+    <!-- Optional: include a polyfill for ES6 Promises for IE11 and Android browser -->
+    <script src="https://jsd.miaowuawa.cn/npm/promise-polyfill"></script>
+    <script charset="utf-8" src="/editor/kindeditor-all.js"></script>
+    <script charset="utf-8" src="/editor/lang/zh-CN.js"></script>
+    <script>
+        KindEditor.ready(function(K) {
+                window.editor = K.create('#editor_id');
+        });
+    </script>
 
     <style>
         html {
@@ -95,6 +117,31 @@
 </head>
 
 <body>
+    <ul class="nav nav-tabs">
+    <div class="navbar-header">
+    <a class="navbar-brand" href="index.php"><?php echo $config["title"]; ?></a>
+    </div>
+
+    <li><a href="index.php?act=home" data-toggle=tab">首页</a></li>
+
+    <li><a href="index.php?act=parts" data-toggle=tab">分区</a></li>
+
+   
+    <?php
+    
+    if(isset($_COOKIE["uid"])){
+        $spacelink=$_COOKIE["uid"];
+        echo "<li><a href=space.php?uid=".$spacelink." data-toggle=tab\">个人空间</a></li>";
+        echo "<li><a href=index.php?act=logout data-toggle=tab\">退出登录</a></li>";
+    }
+    else
+    {
+       echo "<li><a href=\"index.php?act=login\" data-toggle=tab\">登录</a></li>";
+       echo "<li><a href=\"index.php?act=signup\" data-toggle=tab\">注册</a></li>";
+    }
+    ?>
+
+    </ul>
     <?php
 
     // Connect to MySQL
@@ -110,15 +157,12 @@
         <h1><?php echo $config["title"]; ?></h1>
     </div>
 
-    <ul class="sidebar">
-        <li><a href="index.php?act=home">主页</a></li>
-        <li><a href="index.php?act=parts">分区</a></li>
-    </ul>
+   
 
     <div class="main">
         <?php
         // Get action
-        $act = $_GET;
+
         if (array_key_exists("act", $act)) {
             $act = $act["act"];
         } else {
@@ -129,13 +173,6 @@
             echo "<ul class='top-bar'>";
             if (isset($_COOKIE["uid"])) {
                 $login = $_COOKIE["uid"];
-                $sql = "SELECT * FROM users WHERE user_id='$login'";
-                $result = $conn->query($sql);
-                $row = $result->fetch_assoc();
-                $pass_sha256 = $_COOKIE["pass_sha256"];
-                if ($pass_sha256 != $row["password"]) {
-                    $login = "not logged in";
-                }
             } else {
                 $login = "not logged in";
             }
@@ -157,13 +194,8 @@
             // Output each article
             while ($row = $result->fetch_assoc()) {
                 if ($row["floor"] == 0) {
-                    // get user info from database
-                    $user_info = $conn->query("SELECT * FROM users WHERE user_id='$row[user_id]'");
-                    $user_info = $user_info->fetch_assoc();
-
                     echo "<li><a href=\"index.php?act=discuss&id=" . $row["dis_id"] . "\">" .
-                        $row["title"] . "</a> - <a href='space.php?uid=" . $user_info["user_id"] .
-                        "'> " . $user_info["username"] . " </a></li>";
+                        $row["title"] . "</a></li>";
                 }
             }
             echo "</ul>";
@@ -190,8 +222,8 @@
                 $password_sha256 = hash("sha256", $password);
                 if ($password_sha256 == $user_info["password"]) {
                     // Set cookie
+                    // header("location: write_into_cookie.php?id=" . $user_info["user_id"]);
                     setcookie("uid", $user_info["user_id"], time() + 2592000); // 30 days
-                    setcookie("pass_sha256", $user_info["password"], time() + 2592000); // 30 days
                     header("location: index.php?act=home");
                     // Redirect to home
                 } else {
@@ -232,18 +264,20 @@
                     // Get user id
                     $user_id = $conn->query("SELECT MAX(user_id) FROM users")->fetch_assoc()["MAX(user_id)"] + 1;  // Get next user id
                     // Insert user into database
-                    $conn->query("INSERT INTO users (user_id, username, password, email) VALUES ('$user_id', '$username', '$password_sha256', '$email')");
+                    $timestampnow=time();
+                    $conn->query("INSERT INTO `users` (`user_id`, `username`, `password`, `email`, `point`, `qianming`, `ban`, `avatar`, `bantimes`, `isadmin`) VALUES ('$user_id', '$username', '$password_sha256', '$email', '1', '这个人很懒，什么也没有写', '0', 'no', '0', '0')");
                     // Set cookie
                     setcookie("uid", $user_id, time() + 2592000); // 30 days
-                    setcookie("pass_sha256", $password_sha256, time() + 2592000); // 30 days
                     header("location: index.php?act=home");
                 }
             }
         } else if ($act == "create-dis") {
             // Create discuss page
             echo "<form action=\"index.php?act=create-dis-next\" method=\"post\">";
+            echo "<br>";
+            echo "标题:";
             echo "<input type=\"text\" name=\"title\" placeholder=\"标题\"><br>";
-            echo "<textarea name=\"content\" placeholder=\"内容\" rows=20 cols=100></textarea><br>";
+            echo "<textarea id=\"editor_id\" name=\"content\" placeholder=\"内容\" rows=20 cols=100></textarea><br>";
             echo "<input type=\"submit\" value=\"发布\">";
             echo "</form>";
         } else if ($act == "create-dis-next") {
@@ -255,7 +289,9 @@
             // Get discuss id
             $dis_id = $conn->query("SELECT MAX(dis_id) FROM discusses")->fetch_assoc()["MAX(dis_id)"] + 1;  // Get next discuss id
             // Insert discuss into database
-            $conn->query("INSERT INTO discusses (dis_id, user_id, title, text, floor, part_id) VALUES ($dis_id, '$user_id', '$title', '$content', 0, 0)");
+            //get unique_id by $timestamp
+            $timestampn=time();
+            $conn->query("INSERT INTO discusses (dis_id, user_id, title, text, floor, part_id,sendtime) VALUES ($dis_id, '$user_id', '$title', '$content', 0, 0 , $timestampn)");
             // Redirect to home
             header("location: index.php?act=home");
         } else if ($act == "discuss") {
@@ -272,21 +308,20 @@
                     // get username
                     $result_user = $conn->query("SELECT username FROM users WHERE user_id='" . $row["user_id"] . "'");
                     $user_info = $result_user->fetch_assoc();
-                    echo "<p><a href='space.php?uid=" . $user_info["user_id"] . "'>" .
-                        $user_info["username"] . "</p>";
-                    echo "<p>" . htmlentities($row["text"], ENT_QUOTES) . "</p>";
+                    echo "<p>" . $user_info["username"] . "：</p>";
+                    echo $row["text"];
                 } else {
                     echo "<h1>" . $row["title"] . "</h1>";
                     // get username
                     $result_user = $conn->query("SELECT * FROM users WHERE user_id='" . $row["user_id"] . "'");
                     $user_info = $result_user->fetch_assoc();
-                    echo "<p><a href=\"space.php?uid=" . $user_info["user_id"] . "\">作者：" .
-                        $user_info["username"] . "</a></p>";
-                    echo "<p>" . htmlentities($row["text"], ENT_QUOTES) . "</p>";
+                    echo "<p>作者：" . $user_info["username"] . "</p>";
+                    echo $row["text"];
                     echo "<hr>";
+                    echo "<h3>回复主题</h3>";
                     echo "<form action=\"index.php?act=reply-next\" method=\"post\">";
                     echo "<input type=\"hidden\" name=\"dis_id\" value=$dis_id>";
-                    echo "<textarea name=\"content\" placeholder='回复' rows=10 cols=50></textarea><br>";
+                    echo "<textarea id=\"editor_id\" name=\"content\" placeholder='回复' rows=10 cols=50></textarea><br>";
                     echo "<input type=\"submit\" value=\"回复\">";
                     echo "</form>";
                 }
@@ -297,28 +332,46 @@
             $content = $_POST["content"];
             // Get user id
             if (!isset($_COOKIE["uid"])) {
+                echo "<script>alert('请先登录！')</script>";
+                header("location: index.php?act=login");
                 die("请先登录");
             }
+           // if (strpos($content, "<script") !== false) {
+           //     die("请不要使用恶意代码，听我说谢谢你");
+            //}
+
+            // 转义特殊字符
+            //$content = htmlentities($content);
 
             $user_id = $_COOKIE["uid"];
             $floor = $conn->query("SELECT MAX(floor) FROM discusses WHERE dis_id='$dis_id'")->fetch_assoc()["MAX(floor)"] + 1;  // Get next floor
             // Insert discuss into database
-            $conn->query("INSERT INTO discusses (dis_id, user_id, title, text, floor, part_id) VALUES ('$dis_id', '$user_id', 'none', '$content', '$floor', 0)");
+            $timestampnow2=time();
+            $conn->query("INSERT INTO discusses (dis_id, user_id, title, text, floor, part_id, sendtime) VALUES ('$dis_id', '$user_id', 'none', '$content', '$floor', 0, $timestampnow2)");
 
             echo $_COOKIE["uid"];
             // Redirect to the discuss
             header("location: index.php?act=discuss&id=$dis_id");
-        } else if ($act == "user") {
-            // Show space page
-            $uid = $_GET["uid"];
-            // Jump to space.php
-            header("location: space.php?uid=$uid");
         }
         ?>
     </div>
-    <h6>Thanks for <a href="https://github.com/Normal-pcer">Normal-pcer</a> and
-        <a href="https://github.com/quqi2">quqi2</a>
-    </h6>
 </body>
+<footer>
+    <div class="container-fluid" style="padding-top: 40px;">
+    <div class="row clearfix">
+        <div class="col-md-12 column">
+            <div class="jumbotron">
+                <div class="container">
+                    <center>
 
+                        <p>Copyright© 2022 All Rights Reserved. </p>
+                        <p>正在开发中</p>
+                    </center>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</footer>
 </html>
