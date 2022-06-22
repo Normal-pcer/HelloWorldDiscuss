34e328cf2c96166c5311b105e3c60382367d6bbf
root <root@miaowuawa.cn>
2022-06-22 21:48:00 +0800

    ip属地

--- funcs.php
+++ funcs.php
@@ -68,3 +68,28 @@ function is_password_true($password, $real)
 {
     return encode_pass($password) == $real;
 }
+
+function get_ip_location($ipaddress){
+    //这一段直接抄csdn上的了
+    $ch = curl_init();
+    $url = 'https://whois.pconline.com.cn/ipJson.jsp?ip='.$ipaddress;
+    curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	//https SSL
+	curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, false );
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+	//获取
+    $location = curl_exec($ch);
+    //至此已经获得ip属地，关闭连接
+	curl_close($ch);
+    //转码
+    $location = mb_convert_encoding($location, 'utf-8','GB2312');
+    $location = substr($location, strlen('({')+strpos($location, '({'),(strlen($location) - strpos($location, '})'))*(-1));
+	//将截取的字符串$location中的‘，’替换成‘&’   将字符串中的‘：‘替换成‘=’
+	$location = str_replace('"',"",str_replace(":","=",str_replace(",","&",$location)));
+	//php内置函数，将处理成类似于url参数的格式的字符串  转换成数组
+	parse_str($location,$ip_location);
+	return $ip_location['addr'];
+
+
+}
