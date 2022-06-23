4fb6a3506c288de92f700929f93564fd1be2815e
root <root@miaowuawa.cn>
2022-06-23 19:03:21 +0800

    auto check updte and more

--- admin.php
+++ admin.php
@@ -8,7 +8,7 @@
         die("请登录后进入后台！");
     }
     else{
-        //写了一半awa
+        //到喵呜服务器，在线检测更新
         $config = json_decode(file_get_contents('config.json'), true);
         $db_host = $config['database.host'];
         $db_name = $config['database.name'];
@@ -43,7 +43,7 @@
     <a href="javascript:location.reload ();" class="mdui-btn mdui-btn-icon">
       <i class="mdui-icon material-icons">refresh</i>
     </a>
-    <a href="javascript:;" class="mdui-btn mdui-btn-icon">
+    <a href="javascript:developalert();" class="mdui-btn mdui-btn-icon">
       <i class="mdui-icon material-icons">more_vert</i>
     </a>
   </div>
@@ -59,20 +59,35 @@
 <div class=main>
 <br>
 <br>
-<h1>管理后台首页</h1>
+<h1><?php echo $config["title"]."管理后台";?> 首页</h1>
 <?php 
+
     $usingversion=$config["discussversion"];
     echo "<font color=\"#9bef51\" size=4>最新版本:$nowversion</font>";
     if($usingversion==$nowversion){
       echo "<font color=\"#9bef51\" size=4> （您正在使用最新版本）</font>";
     }
     else{
-      echo "<font color=brown size=4> （为确保安全，请尽快升级）</font>";
+      $tmpusingversion=$config["discussversion"];
+      echo "<font color=brown size=4> 您正在使用：$tmpusingversion<br>（为确保安全，请尽快升级）</font>";
     }
 
     echo "<br>";
     echo "<br>";
     echo "<font size=4>注册用户：-位 总发帖量：-篇 主题：-篇";
+    echo "<br>";
+    echo "<br>";
+    echo "<h3>主要设置概览：</h3>";
+    echo "<ul>";
+    if($config["server.salt.enabled"]){
+      echo "<li><font color=\"#9bef51\" size=3 >已启用密码加盐</font></li>";
+    }
+    else{
+      echo "<li><font color=brown size=3>未启用密码加盐</font></li>";
+    }
+    echo "<li><font size=3>php版本号:".substr(PHP_VERSION,0,3)."</font>";
+    echo "</ul>";
+
 
 ?>
 </div>
