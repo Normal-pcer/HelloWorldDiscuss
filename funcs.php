79a1aca45590949ed7ceb4af97de39e55fa4f789
root <root@miaowuawa.cn>
2022-06-23 08:38:13 +0800

    getusrip function

--- funcs.php
+++ funcs.php
@@ -94,6 +94,8 @@ function get_ip_location($ipaddress){
 	//php内置函数，将处理成类似于url参数的格式的字符串  转换成数组
 	parse_str($location,$ip_location);
 	return $ip_location['addr'];
+
+
 }
 
 function get_discuss($dis_id, $floor)
@@ -142,3 +144,24 @@ function del_discuss($dis_id, $floor)
         die("ERR_NOT_OWNER");
     }
 }
+function getusrip(){
+    $unknown = 'unknown';
+    if ( isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'] && strcasecmp($_SERVER['HTTP_X_FORWARDED_FOR'], $unknown) ) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } elseif ( isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], $unknown) ) {
+    $ip = $_SERVER['REMOTE_ADDR'];
+    if (false !== strpos($ip, ','))
+    $ip = reset(explode(',', $ip));
+    return $ip;
+}
+}
+
+/*
+
+处理多层代理的情况
+
+或者使用正则方式：$ip = preg_match("/[\d\.]{7,15}/", $ip, $matches) ? $matches[0] : $unknown;
+
+*/
+
+
