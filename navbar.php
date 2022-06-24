79281b1f7b6724432fb7a2c233b98696fe57f41e
root <root@miaowuawa.cn>
2022-06-24 09:15:53 +0800

    add new login page

--- navbar.php
+++ navbar.php
@@ -15,7 +15,7 @@
             echo "<li><a href=index.php?act=space&uid=" . $spacelink . " data-toggle=tab\">个人空间</a></li>";
             echo "<li><a href=index.php?act=logout data-toggle=tab\">退出登录</a></li>";
         } else {
-            echo "<li><a href=\"index.php?act=login\" data-toggle=tab\">登录</a></li>";
+            echo "<li><a href=\"login.php\" data-toggle=tab\">登录</a></li>";
             echo "<li><a href=\"index.php?act=signup\" data-toggle=tab\">注册</a></li>";
         }
         ?>
