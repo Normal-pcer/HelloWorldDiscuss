32461f8152357c148275e1c3fd801ea68be65674
normal-pcer <tianyafei2009@outlook.com>
2022-07-17 19:32:08 +0800

    欸嘿嘿

--- discuss.php
+++ discuss.php
@@ -18,11 +18,23 @@ $discusses = $discusses->fetch_assoc();
 <body>
     <?php
     echo "<h1>" . $discusses["discussionname"] . "</h1>";
+    echo "<p> 作者: " . GetUserInfo("user_id", $discusses["user_id"])["username"] . "</p>";
     echo "<article>";
     echo str_replace("\n", "<br>", $discusses["content"]);
     echo "</article>";
     ?>
     <hr>
+    <?php
+    $sql = "SELECT * FROM `discusses` WHERE `discussion_id` = " . $discuss_id . " AND `floor` > 1";
+    $discusses = $conn->query($sql);
+    while ($row = $discusses->fetch_assoc()) {
+        echo "<p> " . GetUserInfo("user_id", $row["user_id"])["username"] . " 的回复 </p>";
+        echo "<article>";
+        echo str_replace("\n", "<br>", $row["content"]);
+        echo "</article><br>";
+    }
+
+    ?>
 </body>
 
 </html>
