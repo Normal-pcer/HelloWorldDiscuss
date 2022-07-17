8e6c94c72a779b63acc4d64cfdc847f12a48dd6f
normal-pcer <tianyafei2009@outlook.com>
2022-07-17 17:33:21 +0800

    Fxxking test

--- discuss.php
+++ discuss.php
@@ -2,18 +2,27 @@
 require "funcs.php";
 $conn = GetConnection();
 $discuss_id = $_GET['aid'];
-$sql = "SELECT * FROM `discusses` WHERE `discuss_id` = " . $discuss_id;
+$sql = "SELECT * FROM `discusses` WHERE `discussion_id` = " . $discuss_id . " AND `floor` = 1";
 $discusses = $conn->query($sql);
 $discusses = $discusses->fetch_assoc();
-echo array_count_values($discusses);
 ?>
 
 <html lang="zh-cn">
 
 <head>
     <title>
-        <?php echo $discusses[0]["title"] . GetConfig()["websiteSetting"]["title"]; ?>
+        <?php echo $discusses["discussionname"] . " - " . GetConfig()["websiteSetting"]["title"]; ?>
     </title>
 </head>
+
+<body>
+    <?php
+    echo "<h1>" . $discusses["discussionname"] . "</h1>";
+    echo "<article>";
+    echo str_replace("\n", "<br>", $discusses["content"]);
+    echo "</article>";
+    ?>
+    <hr>
+</body>
 
 </html>
